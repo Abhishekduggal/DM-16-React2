0eaad73f8ca5124ce504077cf96583bb97787fd9
Erik Golden <erikgolden7@gmail.com>
2018-06-26 14:58:51 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Child from './Child';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      number: 0
+    };
+    this.increaseNum = this.increaseNum.bind(this);
+  }
+
+  increaseNum() {
+    this.setState({ number: this.state.number + 1 });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <Child name={'Shea'} num={this.state.number} add={this.increaseNum} />
+      </div>
+    );
+  }
+}
+
+export default App;
